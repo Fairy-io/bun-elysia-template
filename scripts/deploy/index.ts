c491294b6991296b69b15bb8a03ab764df46072a
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-14 14:45:28 +0100

    printing envs in deploy script

--- scripts/deploy/index.ts
+++ scripts/deploy/index.ts
@@ -1,1 +1,10 @@
-console.log(process.env);
+import { validateObject } from '../../src/utils/validateObject';
+import { envSchema } from './envSchema';
+
+const deploy = async () => {
+    const envs = validateObject(process.env, envSchema);
+
+    console.log(envs);
+};
+
+deploy();
