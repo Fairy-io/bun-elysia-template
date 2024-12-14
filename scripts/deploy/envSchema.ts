166ad1caaa4ca9559accd6d9a0dbbec68a741c30
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-14 14:45:09 +0100

    adding envSchema for deploy script

---
+++ scripts/deploy/envSchema.ts
@@ -0,0 +1,14 @@
+import { t } from 'elysia';
+
+export const envSchema = t.Object({
+    _IMAGE_NAME: t.String(),
+    _IMAGE_TAG: t.String(),
+    _DEPLOY_NAME: t.String(),
+    _DEPLOY_INFRA_REPO_OWNER: t.String(),
+    _DEPLOY_INFRA_REPO: t.String(),
+    _DEPLOY_PATH: t.String(),
+    _DEPLOY_TEMPLATE: t.String(),
+    BRANCH_NAME: t.String(),
+});
+
+export type EnvSchema = typeof envSchema.static;
