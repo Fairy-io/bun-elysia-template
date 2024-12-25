719b692c8a0e719180d1ab587b560911f2eef060
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-26 00:14:36 +0100

    adding documentation environment variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,9 @@ import { parseIntObject } from '../utils/parseIntObject';
 
 const configSchema = t.Object({
     PORT: t.Optional(t.Numeric()),
+    DOCUMENTATION_TITLE: t.Optional(t.String()),
+    DOCUMENTATION_VERSION: t.Optional(t.String()),
+    DOCUMENTATION_DESCRIPTION: t.Optional(t.String()),
 });
 
 export const config = validateObject(
