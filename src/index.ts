fb2ca0c014a24ba9eaf30baeb9dbf10787d54a70
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-26 00:15:29 +0100

    adding swagger plugin

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import { Elysia } from 'elysia';
+import { swagger } from '@elysiajs/swagger';
 import { config } from './config';
 
 const app = new Elysia()
+    .use(
+        swagger({
+            documentation: {
+                info: {
+                    title: config.DOCUMENTATION_TITLE,
+                    version: config.DOCUMENTATION_VERSION,
+                    description:
+                        config.DOCUMENTATION_DESCRIPTION,
+                },
+            },
+            path: '/docs',
+        }),
+    )
     .get('/', () => 'Hello Elysia')
-    .listen(config.PORT || 3000);
+    .listen(config.PORT);
 
 console.log(
     `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
