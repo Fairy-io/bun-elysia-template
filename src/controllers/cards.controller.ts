8864fe080b9806d1aa1791ec142cb28473bcd9b0
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-26 01:14:31 +0100

    updating cards controller for using models

--- src/controllers/cards.controller.ts
+++ src/controllers/cards.controller.ts
@@ -1,22 +1,97 @@
-import Elysia from 'elysia';
+import Elysia, { t } from 'elysia';
+import { ElysiaCustomStatusResponse } from 'elysia/dist/error';
 import { ElysiaCommon } from '../common';
+import { CardDto } from '../models/api/dto';
+import { CardModel } from '../models/api/response';
+
+const CardsModel = t.Array(CardModel);
 
 export const CardsController = new Elysia({
     prefix: '/cards',
 })
     .use(ElysiaCommon)
-    .get('', async () => 'cards', {
-        detail: { description: 'Get all cards' },
-    })
-    .get(':id', async () => 'card', {
-        detail: { description: 'Get card by id' },
-    })
-    .post('', async () => 'created card', {
-        detail: { description: 'Create card' },
-    })
-    .put(':id', async () => 'updated card', {
-        detail: { description: 'Update card by id' },
-    })
-    .delete(':id', async () => 'deleted card', {
-        detail: { description: 'Delete card by id' },
-    });
+
+    .model('dto', CardDto)
+    .model('card', CardModel)
+    .model('cards', CardsModel)
+
+    .get(
+        '',
+        async (): Promise<
+            | typeof CardsModel.static
+            | ElysiaCustomStatusResponse<any>
+        > => {
+            return [];
+        },
+        {
+            detail: { description: 'Get all cards' },
+            response: {
+                200: 'cards',
+            },
+        },
+    )
+
+    .get(
+        ':id',
+        async (): Promise<
+            | typeof CardModel.static
+            | ElysiaCustomStatusResponse<any>
+        > => {
+            return { name: '123' };
+        },
+        {
+            detail: { description: 'Get card by id' },
+            response: { 200: 'card' },
+        },
+    )
+
+    .post(
+        '',
+        async ({
+            set,
+        }): Promise<
+            | typeof CardModel.static
+            | ElysiaCustomStatusResponse<any>
+        > => {
+            set.status = 201;
+
+            return { name: '123' };
+        },
+        {
+            detail: { description: 'Create card' },
+            body: 'dto',
+            response: {
+                201: 'card',
+            },
+        },
+    )
+
+    .put(
+        ':id',
+        async (): Promise<
+            | typeof CardModel.static
+            | ElysiaCustomStatusResponse<any>
+        > => {
+            return {
+                name: '123',
+            };
+        },
+        {
+            detail: { description: 'Update card by id' },
+            body: 'dto',
+            response: {
+                200: 'card',
+            },
+        },
+    )
+
+    .delete(
+        ':id',
+        async (): Promise<void | ElysiaCustomStatusResponse<any>> => {},
+        {
+            detail: { description: 'Delete card by id' },
+            response: {
+                204: 'void',
+            },
+        },
+    );
