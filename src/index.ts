eb9929f2082ec441649a5e7ebf48e57bd03cc971
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-27 23:21:40 +0100

    using create app function for initialize app

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,9 @@
-import { Elysia } from 'elysia';
-import { swagger } from '@elysiajs/swagger';
 import { config } from './config';
-import { CardsController } from './controllers';
+import { createApp } from './createApp';
 
-const app = new Elysia()
-    .use(
-        swagger({
-            documentation: {
-                info: {
-                    title: config.DOCUMENTATION_TITLE,
-                    version: config.DOCUMENTATION_VERSION,
-                    description:
-                        config.DOCUMENTATION_DESCRIPTION,
-                },
-            },
-            path: '/docs',
-        }),
-    )
-    .use(CardsController)
-    .listen(config.PORT);
+const app = createApp();
+
+app.listen(config.PORT);
 
 console.log(
     `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
