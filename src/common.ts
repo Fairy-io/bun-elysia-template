49254a27c65f375d5512a2ed0d221df90bb23d2d
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-27 23:21:16 +0100

    providing inject for controllers

--- src/common.ts
+++ src/common.ts
@@ -1,10 +1,14 @@
 /**
- * This is common Elysia configuration which can be used for all controlelrs
+ * This is common Elysia configuration which can be used for all controllers
  */
 
 import Elysia from 'elysia';
 import { VoidModel } from './models/api/response';
+import { inject } from './utils/di';
 
 export const ElysiaCommon = new Elysia({
     name: 'common',
-}).model('void', VoidModel);
+})
+    .model('void', VoidModel)
+
+    .decorate('inject', inject);
