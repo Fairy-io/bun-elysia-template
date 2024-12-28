a6151365e15caa76b0872945f5a3ebbcb38f8376
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-28 14:18:44 +0100

    adding positive get card by id test

--- tests/getCard.spec.ts
+++ tests/getCard.spec.ts
@@ -1,3 +1,54 @@
-import { describe } from 'bun:test';
+import {
+    afterEach,
+    beforeAll,
+    describe,
+    expect,
+    it,
+} from 'bun:test';
+import { createApp } from '../src/createApp';
+import { CardsProviderMock } from './mocks/cards.provider';
+import { createCard } from './helpers/createCard';
 
-describe('GET /card/:id (test)', () => {});
+describe('GET /card/:id (test)', () => {
+    let app: ReturnType<typeof createApp>;
+    let CardsProvider: CardsProviderMock;
+
+    beforeAll(() => {
+        CardsProvider = new CardsProviderMock();
+
+        app = createApp({ CardsProvider });
+    });
+
+    afterEach(() => {
+        CardsProvider.fetchCards.mockReset();
+    });
+
+    it('returns card if exists', async () => {
+        const card = {
+            id: '123',
+            name: 'Name',
+            power: 5,
+            description: 'Description',
+            created_at: '2024-12-28T10:59:23.489Z',
+            updated_at: '2024-12-28T10:59:23.489Z',
+        };
+
+        CardsProvider.getCardById.mockReturnValue(
+            createCard(card),
+        );
+
+        const response = await app
+            .handle(
+                new Request('http://localhost/cards/123', {
+                    method: 'get',
+                }),
+            )
+            .then((res) => res.json());
+
+        expect(response).toEqual(card);
+
+        expect(
+            CardsProvider.getCardById,
+        ).toHaveBeenCalledWith('123');
+    });
+});
