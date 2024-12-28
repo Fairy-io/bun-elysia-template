44735c471ef6f9ad5feff52640f9ea26a9e6ab6b
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-28 12:02:50 +0100

    providing cards provider

--- src/createApp.ts
+++ src/createApp.ts
@@ -3,11 +3,16 @@ import { swagger } from '@elysiajs/swagger';
 import { config } from './config';
 import { CardsController } from './controllers';
 import { provide } from './utils/di';
+import { CardsProvider } from './providers';
 
-export type DiStore = {};
+export type DiStore = {
+    CardsProvider: CardsProvider;
+};
 
 export const createApp = (di: Partial<DiStore> = {}) => {
-    const defaultDiStore: DiStore = {};
+    const defaultDiStore: DiStore = {
+        CardsProvider: new CardsProvider(),
+    };
 
     provide({ ...defaultDiStore, ...di });
 
