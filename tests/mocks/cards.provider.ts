aef6617ca6f792cead5148aabc08d498fb1f5d30
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-12-28 12:03:13 +0100

    adding cards provider mock

---
+++ tests/mocks/cards.provider.ts
@@ -0,0 +1,7 @@
+import { mock } from 'bun:test';
+
+import { CardsProvider } from '../../src/providers';
+
+export class CardsProviderMock implements CardsProvider {
+    public fetchCards = mock();
+}
