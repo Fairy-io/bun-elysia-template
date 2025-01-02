b2b83a8f0836a7f628e4b3924507e5f17f358ebe
massfice <adrian.marian.tomasz.larysz@gmail.com>
2025-01-02 12:11:04 +0100

    changing functions names

--- tests/mocks/cards.provider.ts
+++ tests/mocks/cards.provider.ts
@@ -3,7 +3,7 @@ import { mock } from 'bun:test';
 import { CardsProvider } from '../../src/providers';
 
 export class CardsProviderMock implements CardsProvider {
-    public fetchCards = mock();
-    public getCardById = mock();
+    public fetchAll = mock();
+    public getById = mock();
     public create = mock();
 }
